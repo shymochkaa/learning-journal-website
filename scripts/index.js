417e82fcaafaf813abbe96d5dec50c3c588b4519
Hanna Shymanovych <shimsnov111@gmail.com>
2025-01-06 20:33:15 +0100

    Adjust padding application for opened post layout

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,24 @@
-const viewMoreBtn = document.querySelector('#view-more-btn')
-const hiddenPosts = document.querySelectorAll('.hidden')
+const viewMoreBtn = document.querySelector("#view-more-btn");
+const hiddenPosts = document.querySelectorAll(".hidden");
 
-viewMoreBtn.addEventListener('click', () => {
-    
+viewMoreBtn.addEventListener("click", () => {
   hiddenPosts.forEach((post, index) => {
     if (index < 3) {
-      post.classList.toggle('hidden')
-      
-    } 
-  })
+      post.classList.toggle("hidden");
+    }
+  });
 
-  const remainingHiddenPosts = document.querySelectorAll('.hidden')
+  const remainingHiddenPosts = document.querySelectorAll(".hidden");
 
   if (remainingHiddenPosts.length > 0) {
-    viewMoreBtn.textContent = 'View more'
+    viewMoreBtn.textContent = "View more";
   } else {
-    viewMoreBtn.textContent = 'View less'
+    viewMoreBtn.textContent = "View less";
   }
- 
-})
+});
 
+const heroContainer = document.querySelector("#hero-container");
 
-const heroContainer = document.querySelector('#hero-container')
-
-heroContainer.addEventListener('click', () => {
-    window.location.href = "post-0.html"; 
-})
+heroContainer.addEventListener("click", () => {
+  window.location.href = "post-0.html";
+});
